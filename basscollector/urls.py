11d4743951ed261524db4043487f827a615009dc
pcheung <pika206@gmail.com>
2022-11-03 16:05:17 -0500

    setup main app urls home page url

--- basscollector/urls.py
+++ basscollector/urls.py
@@ -12,10 +12,14 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+
+    #! this is the project urls file - be careful 
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('', include('main_app.urls')),
 ]
