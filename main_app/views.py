0f0d5e5c67891a92da9d3157746fa1cacdfb3e4c
pcheung <pika206@gmail.com>
2022-11-04 16:40:52 -0500

    implement bass model and register admin

--- main_app/views.py
+++ main_app/views.py
@@ -1,23 +1,23 @@
 from django.shortcuts import render
-
+from .models import Bass
 
 
 # Create your views here.
 
-class Bass:
-  def __init__(self, manufacturer, model, strings, image):
-    self.manufacturer = manufacturer
-    self.model = model
-    self.strings = strings
-    self.image = image
+# class Bass:
+#   def __init__(self, manufacturer, model, strings, image):
+#     self.manufacturer = manufacturer
+#     self.model = model
+#     self.strings = strings
+#     self.image = image
 
-basses = [
-  Bass('Fender', 'Precision', 4, 'fender-precision-200.png'),
-  Bass('Fender', 'Jazz', 4, 'fender-jazz-200.png'),
-  Bass('Musicman', 'Stingray 5', 5, 'eb-stingray5-200.png'),
-  Bass('Warwick', 'FNA Jazzman', 5, 'warwick-fna-200.png'),
-  Bass('Gibson', 'The Ripper', 4, 'gibson-ripper-200.png'),
-]
+# basses = [
+#   Bass('Fender', 'Precision', 4, 'fender-precision-200.png'),
+#   Bass('Fender', 'Jazz', 4, 'fender-jazz-200.png'),
+#   Bass('Musicman', 'Stingray 5', 5, 'eb-stingray5-200.png'),
+#   Bass('Warwick', 'FNA Jazzman', 5, 'warwick-fna-200.png'),
+#   Bass('Gibson', 'The Ripper', 4, 'gibson-ripper-200.png'),
+# ]
 
 
 
@@ -32,4 +32,5 @@ def about(request):
 
 # basses view
 def basses_index(request):
+  basses = Bass.objects.all()
   return render(request, 'basses/index.html', {'basses':basses})
