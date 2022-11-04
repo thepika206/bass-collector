2fff0c57966c3aff56a776cc883e6613f75431ca
pcheung <pika206@gmail.com>
2022-11-03 21:55:44 -0500

    implement bass index page with pictures

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
   path('', views.home, name='home'),
+  path('basses/', views.basses_index, name='basses_index'),
   path('about/', views.about, name='about'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -4,6 +4,23 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+class Bass:
+  def __init__(self, manufacturer, model, strings, image):
+    self.manufacturer = manufacturer
+    self.model = model
+    self.strings = strings
+    self.image = image
+
+basses = [
+  Bass('Fender', 'Precision', 4, 'fender-precision-200.png'),
+  Bass('Fender', 'Jazz', 4, 'fender-jazz-200.png'),
+  Bass('Musicman', 'Stingray 5', 5, 'eb-stingray5-200.png'),
+  Bass('Warwick', 'FNA Jazzman', 5, 'warwick-fna-200.png'),
+  Bass('Gibson', 'The Ripper', 4, 'gibson-ripper-200.png'),
+]
+
+
+
 # home view
 def home(request):
   return HttpResponse('<h1>hello bass</h1>')
@@ -11,4 +28,7 @@ def home(request):
 # about view
 def about(request):
   return render(request, 'about.html')
-  # return HttpResponse('<h1>About the bass collector</h1>')
+
+# basses view
+def basses_index(request):
+  return render(request, 'basses/index.html', {'basses':basses})
