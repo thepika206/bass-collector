9c0576bccdd6da73f3784c0c40d625dfac40bb37
pcheung <pika206@gmail.com>
2022-11-04 17:27:39 -0500

    impl bass details page

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+  #? path('[http url]', views.[where in the views file], name='[reference this route from templates]' )
   path('', views.home, name='home'),
-  path('basses/', views.basses_index, name='basses_index'),
   path('about/', views.about, name='about'),
+  path('basses/', views.basses_index, name='basses_index'),
+  path('basses/<int:bass_id>/', views.basses_detail, name='basses_detail')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,26 +1,6 @@
 from django.shortcuts import render
 from .models import Bass
 
-
-# Create your views here.
-
-# class Bass:
-#   def __init__(self, manufacturer, model, strings, image):
-#     self.manufacturer = manufacturer
-#     self.model = model
-#     self.strings = strings
-#     self.image = image
-
-# basses = [
-#   Bass('Fender', 'Precision', 4, 'fender-precision-200.png'),
-#   Bass('Fender', 'Jazz', 4, 'fender-jazz-200.png'),
-#   Bass('Musicman', 'Stingray 5', 5, 'eb-stingray5-200.png'),
-#   Bass('Warwick', 'FNA Jazzman', 5, 'warwick-fna-200.png'),
-#   Bass('Gibson', 'The Ripper', 4, 'gibson-ripper-200.png'),
-# ]
-
-
-
 # home view
 def home(request):
   return render(request, 'home.html')
@@ -34,3 +14,7 @@ def about(request):
 def basses_index(request):
   basses = Bass.objects.all()
   return render(request, 'basses/index.html', {'basses':basses})
+
+def basses_detail(request, bass_id):
+  bass = Bass.objects.get(id=bass_id)
+  return render(request, 'basses/detail.html', {'bass':bass})
